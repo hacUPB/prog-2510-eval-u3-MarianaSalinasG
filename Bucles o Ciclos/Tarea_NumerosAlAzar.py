f56cbfcf08d27be05c991fa77823103babce77ec
Mariana Salinas <mariana.salinasg@upb.edu.co>
2025-03-17 20:51:49 -0500

    Tarea de clase

---
+++ Bucles o Ciclos/Tarea_NumerosAlAzar.py
@@ -0,0 +1,8 @@
+# Crear una lista vacía y llenar esa lista con 100 números al azar entre 1 y 100.
+import random
+i = 0
+lista = []
+
+for i in range(0, 100):
+    lista.append(random.randint(1, 100))
+    print(f"{i} = {lista}")    
