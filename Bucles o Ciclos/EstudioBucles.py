5c598adbfff2668ebbee0f39e8e7d74187c263a7
Mariana Salinas <mariana.salinasg@upb.edu.co>
2025-03-25 15:16:23 -0500

    Agregué el código del quiz

---
+++ Bucles o Ciclos/EstudioBucles.py
@@ -0,0 +1,5 @@
+canciones = ["Esclava de sus besos", "Las mujeres están de moda", "Un Zombie a la interperie", "Tu te imaginas", "Con Bochinche"]
+artistas = ["Guido G", "Grupo Niche", "Alejandro Sanz y Luis Enrique", "De la Ghetto", "Sech"]
+
+for cancion in canciones:
+    print(cancion)
